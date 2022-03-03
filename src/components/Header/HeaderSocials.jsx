2c675c73232511d8e8d7534cd2ed5cb58d7995d8
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-03 20:14:15 -0300

    📜Feat: Using react-icons to show social icons

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a
-        href="https://linkedin.com/feed/"
-        target={"_blank"}
-        rel="noreferrer"
-      ></a>
-      <a href="https://github.com/" target={"_blank"} rel="noreferrer"></a>
-      <a href="https://instagram.com/" target={"_blank"} rel="noreferrer"></a>
+      <a href="https://linkedin.com/feed/" target={"_blank"} rel="noreferrer">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/" target={"_blank"} rel="noreferrer">
+        <FaGithub />
+      </a>
+      <a href="https://instagram.com/" target={"_blank"} rel="noreferrer">
+        <BsInstagram />
+      </a>
     </div>
   );
 };
