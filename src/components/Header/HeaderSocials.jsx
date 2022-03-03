51c29452aa596934904572d11f9889930d29d434
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-03 20:02:56 -0300

    📜Feat: implementing Socials buttons

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const HeaderSocials = () => {
-  return <div>HeaderSocials</div>;
+  return (
+    <div className="header__socials">
+      <a
+        href="https://linkedin.com/feed/"
+        target={"_blank"}
+        rel="noreferrer"
+      ></a>
+      <a href="https://github.com/" target={"_blank"} rel="noreferrer"></a>
+      <a href="https://instagram.com/" target={"_blank"} rel="noreferrer"></a>
+    </div>
+  );
 };
 
 export default HeaderSocials;
