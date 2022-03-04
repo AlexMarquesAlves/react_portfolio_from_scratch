2875cbbcbbcac1abaaeaf5de21e33ee001943c1b
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-03 21:06:43 -0300

    📜Feat: Implement navgation button

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,24 @@
+import "./styles.css";
+
 function Nav() {
   return (
-    <h1>Nav</h1>
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
   );
 }
 
