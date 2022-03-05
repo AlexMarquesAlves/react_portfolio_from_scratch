8c7d21f3228b14107e5ff0f23653531ef1ff01c1
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-05 01:51:18 -0300

    📜Feat: Form ended

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -45,7 +45,29 @@ const Contact = () => {
           </article>
         </div>
         {/* END of Contact options */}
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder={`Your Full Name`}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder={`Your Email`}
+            required
+          />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder={`Your Message`}
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
