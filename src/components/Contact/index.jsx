3f2e9f39381baa459743c30ed43e684f01c30a2b
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-05 01:46:32 -0300

    📜Feat: Contact options ended

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { MdOutlineMail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Torch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>dummymail@mail.com</h5>
+            <a
+              href="mailto:dummymail@mail.com"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>dummymessenger</h5>
+            <a href="https://m.me/" target={"_blank"} rel="noreferrer">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>Whatsapp</h4>
+            <h5>+123456789</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=123456789"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Send a Message
+            </a>
+          </article>
+        </div>
+        {/* END of Contact options */}
+        <form action=""></form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
