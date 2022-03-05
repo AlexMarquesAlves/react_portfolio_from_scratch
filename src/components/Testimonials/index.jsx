f3fd26f5e086ced3a2bebc8b35f38ab97409e6b2
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-05 00:18:01 -0300

    🚫Fix: Portfolio conmponent -- content of client__avatar

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -16,53 +16,53 @@ const Testimonials = () => {
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Liza Scout</h5>
-            <small className="client__review">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-              non harum vel, iusto, laboriosam dolor autem aliquid commodi
-              tempore aut velit, fugiat consequatur fuga sunt earum id sapiente
-              consequuntur neque.
-            </small>
           </div>
+          <h5 className="client__name">Liza Scout</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+            harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+            velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+            neque.
+          </small>
         </article>
 
         <article className="testimonial">
           <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Liza Scout</h5>
-            <small className="client__review">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-              non harum vel, iusto, laboriosam dolor autem aliquid commodi
-              tempore aut velit, fugiat consequatur fuga sunt earum id sapiente
-              consequuntur neque.
-            </small>
+            <img src={AVTR2} alt="Avatar One" />
           </div>
+          <h5 className="client__name">Liza Scout</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+            harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+            velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+            neque.
+          </small>
         </article>
 
         <article className="testimonial">
           <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Liza Scout</h5>
-            <small className="client__review">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-              non harum vel, iusto, laboriosam dolor autem aliquid commodi
-              tempore aut velit, fugiat consequatur fuga sunt earum id sapiente
-              consequuntur neque.
-            </small>
+            <img src={AVTR3} alt="Avatar One" />
           </div>
+          <h5 className="client__name">Liza Scout</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+            harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+            velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+            neque.
+          </small>
         </article>
 
         <article className="testimonial">
           <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className="client__name">Liza Scout</h5>
-            <small className="client__review">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
-              non harum vel, iusto, laboriosam dolor autem aliquid commodi
-              tempore aut velit, fugiat consequatur fuga sunt earum id sapiente
-              consequuntur neque.
-            </small>
+            <img src={AVTR4} alt="Avatar One" />
           </div>
+          <h5 className="client__name">Liza Scout</h5>
+          <small className="client__review">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+            harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+            velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+            neque.
+          </small>
         </article>
       </div>
     </section>
