d4b05e3bd84136dec34c36cb372aab91e8a83311
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-05 00:33:17 -0300

    📜Feat: Testimonials component -- data to map

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -6,6 +6,42 @@ import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
 
+// Testimonials Data
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+  {
+    avatar: AVTR2,
+    name: "Shatta Wale",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+  {
+    avatar: AVTR4,
+    name: "Nana Ama McBrown",
+    review: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error non
+    harum vel, iusto, laboriosam dolor autem aliquid commodi tempore aut
+    velit, fugiat consequatur fuga sunt earum id sapiente consequuntur
+    neque.`,
+  },
+];
+
 const Testimonials = () => {
   return (
     <section id="testimonials">
