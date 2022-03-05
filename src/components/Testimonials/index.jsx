ed6eb97f506b0e7aa38c0213658f49a92d748975
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-05 01:07:54 -0300

    📜Feat: Testimonials component -- using Swiper & SwiperSlide

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -7,9 +7,13 @@ import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
 
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Pagination } from "swiper";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 // Testimonials Data
 const data = [
@@ -53,19 +57,30 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper
+        className="container testimonials__container"
+        // Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        // navigation
+        // scrollbar={{ draggable: true }}
+        // onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log("slide change")}
+      >
         {data.map(({ avatar, name, review }, index) => {
           return (
-            <article key={index} className="testimonial">
+            <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt={name} />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
