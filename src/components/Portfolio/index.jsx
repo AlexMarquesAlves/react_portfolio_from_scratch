b3697e96bcf9b43ca50a66b166bfffa1bbce8195
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-04 23:18:48 -0300

    📜Feat: Portfolio component structure

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+          <h3>This is a Portfolio Item Title</h3>
+          <a
+            href="https://github.com/"
+            className="btn"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/"
+            className="btn btn-primary"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
