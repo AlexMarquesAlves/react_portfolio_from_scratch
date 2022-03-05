08bf669e03869ec584b2b1ce51ccf6917edc17d1
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-05 15:21:29 -0300

    Feat: using Scss on Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles.css";
+import "./styles.scss";
 import CTA from "./CTA";
 import ME from "../../assets/me.jpg";
 import HeaderSocials from "./HeaderSocials";
