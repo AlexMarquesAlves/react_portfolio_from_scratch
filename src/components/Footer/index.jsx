e3f29f2abdea2ac0d64b23cc5362220e1fe5ce6a
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-03-05 14:17:23 -0300

    📜Feat: Footer structure ended

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,57 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+// React icons imports
+import { FaFacebookkf, FaTwitter } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        ¡Duque
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookkf />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FaTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; ¡Duque. All rights reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
